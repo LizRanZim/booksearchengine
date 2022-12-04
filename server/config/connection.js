67625d1c99ba1251bd6e7465a2c33ae0937bee41
LizRanZim <lrzclass22@gmail.com>
2022-12-04 16:49:16 -0500

    deploying

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,10 +1,21 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false,
-});
+// *** Heroku deploy db connection
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/googlebooks',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  },
+);
+
+
+// *** Local connection to db
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
+//   useFindAndModify: false,
+// });
 
 module.exports = mongoose.connection;
