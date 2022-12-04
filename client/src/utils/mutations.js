a622ccac89d063e37b387147cc717e120d86ab42
LizRanZim <lrzclass22@gmail.com>
2022-12-04 15:47:31 -0500

    Update mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -5,7 +5,7 @@ export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
       token
-      user {
+      profile {
         _id
         username
       }
@@ -17,7 +17,7 @@ export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
       token
-      user {
+      profile {
         _id
         username
       }
